024d9f6d75c2256afb63e85d33449834d37b22f4
bhlaing <billhlaing@gmail.com>
2021-06-16 11:07:10 +1000

    Removed unused file

--- app/src/main/java/com/billy/starships/shared/ResultInteractor.kt
+++
@@ -1,9 +0,0 @@
-package com.billy.starships.shared
-
-abstract class ResultInteractor<in P, R> {
-    suspend operator fun invoke(params: P): R {
-        return doWork(params)
-    }
-
-    protected abstract suspend fun doWork(params: P): R
-}
